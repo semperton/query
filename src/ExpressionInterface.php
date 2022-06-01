3127228467d6ecebeb06ccbb92897ae2cb5a86d6
Johannes Rabausch <mail@jrabausch.de>
2022-06-01 07:43:27 +0000

    Code clean-up

--- src/ExpressionInterface.php
+++ src/ExpressionInterface.php
@@ -7,7 +7,11 @@ namespace Semperton\Query;
 interface ExpressionInterface
 {
 	public function valid(): bool;
-	/** @return self */
+
+	/**
+	 * @return self
+	 */
 	public function reset();
+
 	public function compile(?array &$params): string;
 }
